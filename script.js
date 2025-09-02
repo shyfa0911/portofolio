b0ef0c523a1963bc29d7ebc9abd71251ad4fce07
shyfa0911 <shyfafelian700@gmail.com>
2025-09-02 12:28:16 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+    
+        var typed = new Typed ('.akusipa',{
+            strings : ['Data Analyst', 'Ui/UX Designer'],
+            typeSpeed : 100,
+            backSpeed : 100,
+            loop : true,
+        });
